b1bfc6b22c77a35768e0f2ffffebdcdc3b2af0ea
1305581 <dem.hadjicosti@outlook.com>
2023-04-06 12:52:11 +0200

    Final Code

---
+++ Final Code/js/main.js
@@ -0,0 +1,38 @@
+function loadApp() {
+
+	// Create the flipbook
+
+	$('.flipbook').turn({
+			// Width
+
+			width:922,
+			
+			// Height
+
+			height:600,
+
+			// Elevation
+
+			elevation: 50,
+			
+			// Enable gradients
+
+			gradients: true,
+			
+			// Auto center this flipbook
+
+			autoCenter: true
+
+	});
+}
+
+// Load the HTML4 version if there's not CSS transform
+
+yepnope({
+	test : Modernizr.csstransforms,
+	yep: ['turnjs4/lib/turn.js'],
+	nope: ['turnjs4/lib/turn.html4.min.js'],
+	both: ['css/style.css'],
+	complete: loadApp
+});
+
